dee947952585ab0d50a2acc1d6380a4981d7fc71
ZaraArican <44377948+ZaraArican@users.noreply.github.com>
2018-12-10 09:10:21 -0500

    Create Deletefromcart.php

---
+++ Deletefromcart.php
@@ -0,0 +1,21 @@
+<?php
+include_once "connect.php";
+
+
+$prodid = $_POST["PRODID"];
+$prodpri = $_POST["PRICE"];
+$prodcat = $_POST["CATEGORY"];
+$prodname = $_POST["PNAME"];
+
+$proddel = "DELETE from cart WHERE PRODID=\"$_GET[PRODID]\"";
+
+//Connecting to database and performing proddel func
+if (mysqli_query($conn, $proddel)) {
+    header("Location: deletep.php");
+    echo "Record Deleted";
+}
+else {
+    echo "Record Not Deleted";
+}
+
+?>
